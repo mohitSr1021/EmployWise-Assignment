c4b1842bb904268270dd8f4fe38b54f3728a2bc7
Mohit Singh Rawat <94630639+mohitSr1021@users.noreply.github.com>
2025-03-27 23:58:48 +0530

    add: analytics package for project monitoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
+      <Analytics />
       <Outlet />
     </>
   )
